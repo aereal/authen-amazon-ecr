a2f66263d1981cad76e84716a94e529e2579848c
aereal <aereal@aereal.org>
2022-04-19 12:45:34 +0900

    feat: add cmd/authen-amazon-ecr

---
+++ cmd/authen-amazon-ecr/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"context"
+	"encoding/base64"
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/service/ecr"
+)
+
+func main() {
+	os.Exit(run())
+}
+
+var envCreds = credentials.NewStaticCredentialsProvider(os.Getenv("AWS_ACCESS_KEY_ID"), os.Getenv("AWS_SECRET_ACCESS_KEY"), os.Getenv("AWS_SESSION_TOKEN"))
+
+func run() int {
+	ctx := context.Background()
+	cfg, err := config.LoadDefaultConfig(ctx, config.WithCredentialsProvider(envCreds))
+	if err != nil {
+		fmt.Println(err)
+		return 1
+	}
+	client := ecr.NewFromConfig(cfg)
+	out, err := client.GetAuthorizationToken(ctx, nil)
+	if err != nil {
+		fmt.Println(err)
+		return 1
+	}
+	creds, err := getCredentials(out)
+	if err != nil {
+		fmt.Println(err)
+		return 1
+	}
+	output("username", creds.user)
+	output("password", creds.password)
+	return 0
+}
+
+type ecrCredentials struct {
+	user     string
+	password string
+}
+
+func getCredentials(out *ecr.GetAuthorizationTokenOutput) (*ecrCredentials, error) {
+	if len(out.AuthorizationData) != 1 {
+		return nil, errors.New("malformed output")
+	}
+	authData := out.AuthorizationData[0]
+	if authData.AuthorizationToken == nil {
+		return nil, errors.New("AuthorizationToken is empty")
+	}
+	decoded, err := base64.StdEncoding.DecodeString(*authData.AuthorizationToken)
+	if err != nil {
+		return nil, err
+	}
+	decodedStr := string(decoded)
+	idx := strings.Index(decodedStr, ":")
+	if idx == -1 {
+		return nil, errors.New("malformed AuthorizationToken")
+	}
+	return &ecrCredentials{user: decodedStr[0:idx], password: decodedStr[idx+1:]}, nil
+}
+
+func output(name, value string) {
+	fmt.Fprintf(os.Stdout, "::set-output name=%s::%s\n", name, value)
+}
